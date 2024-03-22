5624cf16edf23beabc08dfa0937abe69fd162b98
Raaaaphh <raphmangini@gmail.com>
2024-03-22 09:12:29 +0100

    askip

--- frontend/meta/authors.ts
+++ frontend/meta/authors.ts
@@ -1,5 +1,5 @@
 // Noms et numéros d'étudiant
-export const authors = ["John Doe (12345678)", "Jane Doe (12345678)"];
+export const authors = ["Mangini Raphael (12208737)", "Monceau Maxime (12201845)"];
 
 // Groupe
-export const group = 0;
+export const group = 1;
